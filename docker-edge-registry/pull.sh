431a2d41315a50abecde71b59896bf6121699a02
Tom <tom_hejiaxi@163.com>
2020-05-27 22:07:50 +0800

    优化边缘镜像推送逻辑

--- docker-edge-registry/pull.sh
+++ docker-edge-registry/pull.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# src0='k8s.gcr.io'
-# src1='kube-controller-manager:v1.18.3'
+# ./pull.sh tomhjx/k8s.gcr.io_kube-controller-manager:v1.18.3 
 # tomhjx/k8s.gcr.io_kube-controller-manager:v1.18.3 => k8s.gcr.io/kube-controller-manager:v1.18.3
 
-src0=$1
-src1=$2
-edge="tomhjx/${src0}_${src1}"
-docker pull  ${edge}
-docker tag ${edge} ${src0}/${src1}
+
+src=$1
+edge0=${src////_}
+edge="tomhjx/${edge0}"
+docker pull ${edge}
+docker tag ${edge} ${src}
--- docker-edge-registry/push.sh
+++ docker-edge-registry/push.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
-# src0='k8s.gcr.io'
-# src1='kube-controller-manager:v1.18.3'
+# ./push k8s.gcr.io/kube-controller-manager:v1.18.3 
 # k8s.gcr.io/kube-controller-manager:v1.18.3 => tomhjx/k8s.gcr.io_kube-controller-manager:v1.18.3
 
-src0=$1
-src1=$2
-edge="tomhjx/${src0}_${src1}"
-docker pull ${src0}/${src1}
-docker tag  ${src0}/${src1} ${edge}
+src=$1
+edge0=${src////_}
+edge="tomhjx/${edge0}"
+docker pull ${src}
+docker tag  ${src} ${edge}
 docker push ${edge}
